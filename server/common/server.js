bf78a0a2f9b818f2b3d16a432f67de550b2b2f68
broketech <broketech@randomactual.space>
2020-02-12 23:25:11 -0800

    Set promisc cross origin headers

--- server/common/server.js
+++ server/common/server.js
@@ -26,6 +26,11 @@ export default class ExpressServer {
     app.use(bodyParser.text({ limit: process.env.REQUEST_LIMIT || '100kb' }));
     app.use(cookieParser(process.env.SESSION_SECRET));
     app.use(Express.static(`${root}/public`));
+    app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", '*'); // update to match the domain you will make the request from
+      res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+      next();
+    });
   }
 
   router(routes) {
