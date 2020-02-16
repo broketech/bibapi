c22ed5093dfd4c512d2f235972704451fed9e6c7
broketech <broketech@randomactual.space>
2020-02-16 10:40:20 -0800

    Changed controller to do plain request return

--- server/api/controllers/bibapi/controller.js
+++ server/api/controllers/bibapi/controller.js
@@ -3,7 +3,7 @@ import bibapiService from '../../services/bibapi.service';
 export class Controller {
   namePredict(req, res) {
     bibapiService.namePredict(req.params.nameFragment).then(r => {
-      if (r) res.json(r);
+      if (r) res.send(r);
       else res.status(404).end();
     });
   }
